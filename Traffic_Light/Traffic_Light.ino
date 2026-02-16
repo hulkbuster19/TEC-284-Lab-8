172330eba36df3548e1bf2a8c9a466b2a12f9611
Phillip VanDerBurg <phillipvanderburg@gmail.com>
2026-02-16 15:11:34 -0600

    Built light functionality on button press

--- Traffic_Light/Traffic_Light.ino
+++ Traffic_Light/Traffic_Light.ino
@@ -1,9 +1,46 @@
+int redLED=7;
+int yellowLED=6; 
+int greenLED=5;
+
+int buttonPin=2;
+
+int green=2500;
+int yellow=2500;
+int red=5000;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(redLED,OUTPUT);
+  pinMode(yellowLED,OUTPUT);
+  pinMode(greenLED,OUTPUT);
+  pinMode(buttonPin,INPUT_PULLUP);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (digitalRead(buttonPin)==HIGH){
+    digitalWrite(redLED,LOW);
+    digitalWrite(yellowLED,LOW);
+    digitalWrite(greenLED,HIGH);
+
+    //delay(green);
+  }else{
+    //green
+    digitalWrite(redLED,LOW);
+    digitalWrite(yellowLED,LOW);
+    digitalWrite(greenLED,HIGH);
+    delay(green);
+
+    //yellow
+    digitalWrite(yellowLED,HIGH);
+    digitalWrite(greenLED,LOW);
+    delay(yellow);
+
+    //red
+    digitalWrite(yellowLED,LOW);
+    digitalWrite(redLED,HIGH);
+    delay(red);
+  }
 
 }
